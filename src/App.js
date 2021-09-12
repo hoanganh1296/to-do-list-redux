6793ca7bbabd48e47df13998f480cf6b22e0d50c
NguyenHoangAnhitc96 <nguyenhoanganh.itc96@gmail.com>
2021-09-12 20:37:53 +0700

    enter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import TaskForm from "./components/TaskForm";
+import Control from "./components/Control";
+import TaskList from "./components/TaskList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state ={ 
+      tasks: [] // id: unique, name, status
+    }
+  }
+  onGenerateData = () =>{
+    var tasks = [
+      {
+        id : ,
+        name: 'Học lập trình',
+        status: true
+      },
+      {
+        id : ,
+        name: 'Đi bơi',
+        status: true
+      },
+      {
+        id : ,
+        name: 'Chơi game',
+        status: false
+      }
+    ]
+  }
+
+
+
+  render() {
+    return (
+      <div className="container">
+        <div className="text-center">
+          <h1>Quản Lý Công Việc </h1>
+          <hr />
+        </div>
+        <div className="row">
+          <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+             {/* FORM */}
+             <TaskForm/>
+           </div>
+          {/* TODO List */}
+          <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+            <button type="button" className="btn btn-primary">
+              <span className="fa fa-plus mr-5"></span> Thêm Công Việc
+            </button>
+            <button 
+                type="button" 
+                className="btn btn-danger ml-5"
+                onClick={this.onGenerateData}    >
+              <span className="fa fa-plus mr-5"></span> Generate Data
+            </button>
+            {/* SEARCH AND SORT */}   
+            <Control/>
+            {/* show TODO list   */}
+            <div className="row mt-15">
+              <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+               <TaskList/>
+               </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
